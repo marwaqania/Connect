54aec87f1c4165af5e8013248e031d2dfb7691d6
marwaqania <marwaqania@gmail.com>
2026-05-15 00:32:00 +0700

    Update relasi.js

--- relasi.js
+++ relasi.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://szfcwkyzrezkijemjwll.supabase.co";
-const SUPABASE_KEY = "sb_publishable_6oLDaiJozQQlee8pKTsshw_xBak5lZh";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6ZmN3a3l6cmV6a2lqZW1qd2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTUwMDMsImV4cCI6MjA5NDMzMTAwM30.jXn9Gngy3y-kW9802bIPAMmYgg-ENTw1CulTQZwyX74";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
